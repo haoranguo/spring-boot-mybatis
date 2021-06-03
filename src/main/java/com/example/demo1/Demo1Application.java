edf2c0e48672f3c35cb184f2fb2fe7f5d09d462e
guohaoran <1241639333@qq.com>
2021-06-03 17:51:54 +0800

    2.5.0

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -17,7 +17,6 @@ public class Demo1Application {
     }
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
-        System.out.println(2);
     }
 
 }
